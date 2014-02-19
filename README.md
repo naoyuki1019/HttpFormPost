688884abbd24d4131033680aa25e134981808c41
naoyuki onishi <naoyuki.onishi@gmail.com>
2014-02-19 13:52:01 +0900

    ver1.0

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ HttpFormPost
 ============
 
 HttpFormPOST in PHP
+
+* ver1.0 - 2014/02/19
---
+++ test.php
@@ -0,0 +1,74 @@
+<?php
+
+if ("POST" === $_SERVER["REQUEST_METHOD"]) {
+
+	echo ('$_POST => ' . nl2br(str_replace(" ", "&nbsp", print_r($_POST, true)) . '<br>'));
+	echo ('$_COOKIE => ' . nl2br(str_replace(" ", "&nbsp", print_r($_COOKIE, true)) . '<br>'));
+	echo ('$_SERVER["HTTP_REFERER"] => ' . nl2br(str_replace(" ", "&nbsp", print_r($_SERVER["HTTP_REFERER"], true)) . '<br><br>'));
+	echo ('$_FILES => ' . nl2br(str_replace(" ", "&nbsp", print_r($_FILES, true)) . '<br>'));
+}
+else {
+
+	include "HttpFormPost.php";
+
+	$ins = new HttpFormPost();
+	$ins->set_url((empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+
+	// add_text
+	$ins->add_text("name", "yamada taro");
+	$ins->add_text("age", "20");
+	$ins->add_text("param[]", "param1");
+	$ins->add_text("param[]", "param2");
+
+	// add_keyval_array
+	$keyval = array('pref' => 'tokyo', 'address1' => 'bunkyo');
+	$ins->add_keyval_array("param", $keyval);
+
+	// add_file
+	$ins->add_file("upload_file[]", "abc.txt", "abc1", "text/plain");
+	$ins->add_file("upload_file[]", "abc.txt", "abc2", "text/plain");
+
+	// add_header
+	$ins->add_header("Referer: http://umauma.com");
+	$ins->add_header("Cookie: session_id=7XgoV3iJqVc%2Bus%");
+
+	$response = $ins->submit();
+	if (FALSE === $response) {
+		echo implode("<br>", $ins->errors()) . "<br>";
+	}
+	else {
+		echo $response;
+	}
+
+
+	echo "==========================================================<br>";
+
+	//
+	// error Attachment not found
+	//
+	$ins = new HttpFormPost();
+	$ins->set_url((empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+	$ins->add_file("upload_file2", "missing.txt", "missing", "text/plain");
+	$response = $ins->submit();
+	if (FALSE === $response) {
+		echo implode("<br>", $ins->errors()) . "<br>";
+	}
+	else {
+		echo $response;
+	}
+
+	echo "==========================================================<br>";
+
+	//
+	// error 404
+	//
+	$ins = new HttpFormPost();
+	$ins->set_url("http://aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+	$response = $ins->submit();
+	if (FALSE === $response) {
+		echo implode("<br>", $ins->errors()) . "<br>";
+	}
+	else {
+		echo $response;
+	}
+}
